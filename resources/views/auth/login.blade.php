bad2cf56125b741d6fc464be01008ae0c37f1630
Antopou <gnanantopou@gmail.com>
2025-06-17 09:16:58 +0700

    remove register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -113,7 +113,7 @@
                     <button type="submit" class="btn btn-primary w-100">Login</button>
                 </form>
                 <div class="text-center mt-3">
-                    <a href="{{ route('register') }}">Don’t have an account? Register</a>
+                    {{-- <a href="{{ route('register') }}">Don’t have an account? Register</a> --}}
                 </div>
             </div>
         </div>
